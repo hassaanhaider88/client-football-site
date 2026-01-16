78c513059d46a11bd7eb2c249739d043cadd21a6
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-01-16 19:12:57 +0500

    auth UI ✔

--- app/(Auth)/signup/page.jsx
+++ app/(Auth)/signup/page.jsx
@@ -4,8 +4,10 @@ import React, { useState } from "react";
 
 const SignUpPage = () => {
   const [InputData, setInputData] = useState({
+    name: "",
     email: "",
     password: "",
+    acceptTerms: false,
   });
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,20 @@ const SignUpPage = () => {
         <h2 className="text-3xl font-semibold mb-6 text-center text-gray-200">
           Create Your Ones!
         </h2>
+        <input
+          id="name"
+          value={InputData.name}
+          onChange={(e) => {
+            setInputData((prev) => ({
+              ...prev,
+              name: e.target.value,
+            }));
+          }}
+          className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
+          type="text"
+          placeholder="Enter your Name"
+          required
+        />
         <input
           id="email"
           value={InputData.email}
@@ -48,6 +64,28 @@ const SignUpPage = () => {
           placeholder="Enter your password"
           required
         />
+        <div className="flex mb-2 items-center">
+          <input
+            id="acceptTerms"
+            value={InputData.acceptTerms}
+            onChange={(e) => {
+              setInputData((prev) => ({
+                ...prev,
+                acceptTerms: e.target.checked,
+              }));
+            }}
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset
+            -2 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            type="checkbox"
+            required
+          />
+          <label
+            htmlFor="acceptTerms"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            I accept the terms and conditions.
+          </label>
+        </div>
         <button
           type="submit"
           className="w-fit relative left-1/2 -translate-x-1/2 py-3 px-8 font-semibold mb-3 bg-white hover:bg-gray-300 text-black active:scale-95 transition rounded-full"
